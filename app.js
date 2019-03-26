202939ace77d706ddf11914068de4d6a82acb72c
melissa yauri <yaurimelissa@gmail.com>
2019-03-26 17:05:14 -0500

    incorporando otros estilos

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const slider = document.querySelector('.items');
 let isDown = false;
 let startX;
+// determina coordenadas
 let scrollLeft;
-
 slider.addEventListener('mousedown', (e) => {
   isDown = true;
   slider.classList.add('active');
  startX = e.pageX - slider.offsetLeft;
+ console.log(startX)
   scrollLeft = slider.scrollLeft;
+  console.log(isDown)
 });
 slider.addEventListener('mouseleave', () => {
   isDown = false;
   slider.classList.remove('active');
+  console.log(isDown)
 });
 slider.addEventListener('mouseup', () => {
   isDown = false;
@@ -19,8 +22,10 @@ slider.addEventListener('mouseup', () => {
 });
 slider.addEventListener('mousemove', (e) => {
   if(!isDown) return;
+  console.log(isDown)
   e.preventDefault();
   const x = e.pageX - slider.offsetLeft;
+  console.log(x)
   const walk = (x - startX) * 3;
   slider.scrollLeft = scrollLeft - walk;
   console.log(walk);
